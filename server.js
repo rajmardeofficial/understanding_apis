6a8b5cc5d611974d05ceea538082311f3e46ba52
Raj Marde <rajmarde615@gmail.com>
2021-11-27 18:10:57 +0530

    first change

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/api/:key_secret', (req, res)=>{
+
+    const key = req.params.key_secret
+
+    const data = {
+        weather: 'Good Day',
+        temp: 26,
+        desc: 'Sunny weather'
+    }
+    res.json(data)
+
+})
+
+
+app.listen(3000, ()=>console.log('server started on port 3000')) 
